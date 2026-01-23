8680d7496f49f4d117105abd73c29752b9747435
dnte-pro <kipronoyegon50@gmail.com>
2026-01-23 15:55:59 +0300

    friday commits

---
+++ Day3/task2.py
@@ -0,0 +1,7 @@
+print(10 % 3)
+
+num = int(input("Enter a numer you want to check the modulo: "))
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
---
+++ Day3/task3.py
@@ -0,0 +1,36 @@
+"""
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
+
+maths_score = int(input("What is your maths score? "))
+english_score = int(input("What is your english score? "))
+if maths_score >= 90:
+    if english_score >=90:
+        print("You are good at everything")
+    else:
+        print("You are good at maths")
+else:
+    print("You are good at English")
+"""
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Pay 5 dollars")
+    elif age <= 18:
+        print("pay 7 dollars")
+    else:
+        print("pay 12 dollars")
+else:
+    print("Sorry you have to grow taller before you can ride.")
